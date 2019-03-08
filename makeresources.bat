8776cfad307d2363862444dcbde6cea99f05d272
paulscottrobson <paulscottrobson@gmail.com>
2019-03-08 17:39:40 +0000

    tweaked resources

--- makeresources.bat
+++ makeresources.bat
@@ -2,10 +2,10 @@
 rem
 rem		Builds resources where required and copies them into applications media directory.
 rem
-copy fonts\*.ttf agkbanjo\media
-copy notes\sounds\*.ogg agkbanjo\media\sounds
-cd graphics
+copy resources\fonts\*.ttf agkbanjo\media
+copy resources\notes\sounds\*.ogg agkbanjo\media\sounds
+cd resources\graphics
 python makeatlas.py
-cd ..
-copy graphics\sprites* agkbanjo\media
+cd ..\..
+copy resources\graphics\sprites* agkbanjo\media
 echo "Built resources"
