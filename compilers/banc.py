db5b6e8beb2b42f1be7596126dba096cb3eff049
Paul Robson <paul@robsons.org.uk>
2019-03-14 13:35:08 +0000

    Multi versions and Erbsen frail reinterpreted

--- compilers/banc.py
+++ compilers/banc.py
@@ -43,7 +43,7 @@ class Bar(object):
 		return Bar.fretting
 
 	def setDisplayChord(self,chord):												# set chord displayed
-		self.__default()
+		self.__default()		
 		chord = chord.strip()
 		self.bar[self.pos]["chord"] = chord[0].upper()+chord[1:].lower() if chord != "" else ""
 
@@ -68,10 +68,10 @@ class Bar(object):
 		if self.bar[self.pos] is None:												# set to default empty
 			self.bar[self.pos] = { "play":[None]*5,"chord":None,"modifier":None,"modcount":0 }
 
-	def render(self):
-		return ".".join([self.__renderNote(x) for x in self.bar])					# render and join notes
+	def render(self,modifier):
+		return ".".join([self.__renderNote(x,modifier) for x in self.bar])			# render and join notes
 
-	def __renderNote(self,note):													# render a note
+	def __renderNote(self,note,modifier):											# render a note
 		r = ""	
 		if note is not None:
 			if note["chord"] is not None:											# chord if present
@@ -79,9 +79,13 @@ class Bar(object):
 			if note["play"] is not None:											# add all played notes
 				for s in range(0,5):
 					if note["play"][s] is not None:
-						r = r + chr(s+49)+chr(note["play"][s]+65)
-						if note["modifier"] is not None:							# modifying them.
-							r = r + note["modifier"] * note["modcount"]
+						if modifier == 0:											# render as is.
+							r = r + chr(s+49)+chr(note["play"][s]+65)
+							if note["modifier"] is not None and modifier == 0:		# modifying them.
+								r = r + note["modifier"] * note["modcount"]
+						else:
+							offset = note["modcount"] * (-1 if note["modifier"] == "-" else 1)
+							r = r + chr(s+49)+chr(note["play"][s]+65+offset)
 		return r
 
 Bar.fretting = [ None ] * 5															# Current fretting
@@ -91,37 +95,52 @@ Bar.fretting = [ None ] * 5															# Current fretting
 # ***************************************************************************************************
 
 class Level1Compiler(object):
-	def __init__(self,equates):
+	def __init__(self,equates,modifier):
 		self.equates = equates
 		self.frettingCode = equates["fretting"]
 		self.beats = int(equates["beats"])
 		self.noteCount = int(equates["notes"])
 		self.currentStrings = [ None ] * self.noteCount
 		self.brushCount = int(equates["brush"])
+		self.modifier = modifier
 
 	def translate(self,barDef):
 		self.bar = Bar(self.noteCount)
 		barDef = barDef.lower().strip()
 		while barDef != "":
 			barDef = self.process(barDef).strip()
-		return self.bar.render()
+		return self.bar.render(self.modifier)
 
 	def process(self,df):
 		m = re.match("^(x*)(["+self.frettingCode+"]+)([\\^\\/v]*)(.*)$",df)			# check for note.
 		if m is not None:
 			cString = len(m.group(1))+1												# starting string.
-			self.bar.setCurrentFretting([None]*5)									# clear all fretting.
-			for c in m.group(2):													# fretting.
-				self.bar.setPlay(self.frettingCode.find(c),cString)
+			newFretting = [ None ] * 5 												# new Fretting
+			oldFretting = self.bar.getCurrentFretting()								# current fretting
+			frettingHasChanged = False
+
+			for c in m.group(2):													# process fretting.
+				newFretting[cString-1] = self.frettingCode.find(c)					# set new fretting
+				if newFretting[cString-1] != oldFretting[cString-1]:				# active change.
+					frettingHasChanged = True
 				cString += 1
+
+			for s in range(0,5):
+				if newFretting[s] is not None:
+					self.bar.setPlay(newFretting[s],s+1)
+				else:
+					if frettingHasChanged:
+						self.bar.setPlay(0,s+1,False)
+
 			for c in m.group(3).replace("^","+").replace("v","-"):					# modifiers
 				self.bar.modify(c)
 			self.bar.movePos(2)														# forward 2
 			return m.group(4)
 		#
 		m = re.match("^\((.*?)\)(.*)$",df)											# set chord ?
-		if m is not None:
-			self.bar.setDisplayChord(m.group(1))									# display chord.
+		if m is not None:			
+			if self.modifier != 2:
+				self.bar.setDisplayChord(m.group(1))								# display chord.			
 			key = "chord_"+m.group(1)												# chord definition key
 			assert key in self.equates,"Missing chord "+m.group(1)					# check it
 			chord = [x for x in self.equates[key]]									# split up.
@@ -130,19 +149,21 @@ class Level1Compiler(object):
 			return m.group(2)
 		#
 		if df[0] == "!":															# brush current fretting
-			fretting = self.bar.getCurrentFretting()
-			for i in range(0,self.brushCount):
-				if fretting[i] is not None:
-					self.bar.setPlay(fretting[i],i+1)
-				else:
-					self.bar.setPlay(0,i+1)
+			if self.modifier != 2:
+				fretting = self.bar.getCurrentFretting()
+				for i in range(0,self.brushCount):
+					if fretting[i] is not None:
+						self.bar.setPlay(fretting[i],i+1)
+					else:
+						self.bar.setPlay(0,i+1)
 			self.bar.movePos(2)														# forward 2
 			return df[1:]
 		#
 		if df[0] == ".":
-			self.bar.movePos(-1)													# backward 1
-			self.bar.setPlay(0,5)													# pluck g5
-			self.bar.movePos(1)														# forward 1
+			if self.modifier != 2:
+				self.bar.movePos(-1)												# backward 1
+				self.bar.setPlay(0,5)												# pluck g5
+				self.bar.movePos(1)													# forward 1
 			return df[1:]
 		#
 		if df[0] == "&":
@@ -164,7 +185,7 @@ class BanjoCompiler(object):
 	def __init__(self):
 		pass
 
-	def compile(self,sourceFile,targetFile):
+	def compile(self,sourceFile,targetFile,modifier):
 		sourceFile = sourceFile.replace("/",os.sep)									# Handle slashes.
 		targetFile = targetFile.replace("/",os.sep)
 		#print("Compiling\n\t{0}\n\t{1}".format(sourceFile,targetFile))
@@ -191,7 +212,7 @@ class BanjoCompiler(object):
 		src = [x for x in src if x.find(":=") < 0]									# remove equates.
 
 		if equates["format"] == "1":												# workout the compiler/translator
-			trans = Level1Compiler(equates)
+			trans = Level1Compiler(equates,modifier)
 		else:
 			raise BanjoException("Bad source format "+equates["format"])
 		#print("\tUsing format {0}.".format(equates["format"]))
@@ -238,6 +259,6 @@ class BanjoCompiler(object):
 		return "".join(s)
 
 if __name__ == "__main__":
-	err = BanjoCompiler().compile("./test.banjo","../agkbanjo/media/music/__test.plux")
+	err = BanjoCompiler().compile("./test.banjo","../agkbanjo/media/music/__test.plux",0)
 	if err is not None:
 		print(err)
