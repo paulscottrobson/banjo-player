aa766ff0ee34c4b622d5ecf11e65c45ff57f0204
Paul Robson <paul@robsons.org.uk>
2019-04-04 15:30:15 +0100

    Index building, latest change copied to __test.plux

--- compiler/buildindex.py
+++ compiler/buildindex.py
@@ -9,7 +9,7 @@
 # ***************************************************************************************************
 # ***************************************************************************************************
 
-import re,os,sys
+import re,os,sys,shutil
 
 # ***************************************************************************************************
 #								Index building class
@@ -18,12 +18,18 @@ import re,os,sys
 class BuildIndex(object):
 
 	def build(self,baseDirectory):
+		self.lastMTime = 0
+		self.lastFile = None
+
 		for root,dirs,files in os.walk(baseDirectory):
 			files = [x for x in files if x.endswith(".plux") and not x.startswith("__") and not x.endswith(".txt")]
 			dirs = [x for x in dirs if not x.startswith("__")]
 			files.sort()
 			dirs.sort()
 			self.buildOneIndex(baseDirectory,root,files,dirs)
+
+		if self.lastFile is not None:
+			self.copyFile(self.lastFile,baseDirectory+os.sep+"__test.plux")
 
 	def buildOneIndex(self,stem,root,files,dirs):		
 		root = root.replace("\\","/")
@@ -36,12 +42,21 @@ class BuildIndex(object):
 		for f in files:
 			h.write(f+"\n")
 			h.write(self.process(f[:-5])+"\n")
+			fileName = root + os.sep + f
+			time = os.stat(fileName).st_mtime
+			if time > self.lastMTime:
+				self.lastMTime = time
+				self.lastFile = fileName
 		h.close()
 
 	def process(self,s):
 		s = [x for x in s.replace("_"," ").split(" ") if x != ""]
 		s = [x[0].upper()+x[1:].lower() for x in s]
 		return " ".join(s)
+
+	def copyFile(self,src,tgt):
+		print("Last modified file "+src)
+		shutil.copyfile(src,tgt)		
 
 if __name__ == "__main__":
 	BuildIndex().build("../agkbanjo/media/music")
