35acbe1bd8facac2f7ef287826577e3124718ca7
paulscottrobson <paulscottrobson@gmail.com>
2020-01-05 18:03:46 +0000

    Updated for 2020
    Fixed Clawhammer bugs and got building working.

--- bluegrass/test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-sh buildall.sh
-echo 0 >..\agkbanjo\media\showmenu.txt
-..\agkbanjo\agkbanjo
-echo 1 >..\agkbanjo\media\showmenu.txt
--- old/clawhammer/songtest.sh
+++
@@ -1,4 +0,0 @@
-python song.py
-echo 0 >../agkbanjo/media/showmenu.txt
-../agkbanjo/agkbanjo
-echo 1 >../agkbanjo/media/showmenu.txt
